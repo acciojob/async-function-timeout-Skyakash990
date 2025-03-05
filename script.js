bff6c9c904232236eee505d61de277ab978991fc
Akash Nandoliya <99115930+Skyakash990@users.noreply.github.com>
2025-03-05 21:22:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let text=document.getElementById("text");
+let delay=document.getElementById("delay");
+let btn=document.getElementById("btn");
+let opDiv=document.getElementById("output");
+
+btn.addEventListener("click",()=>{
+	let tx=text.value.trim();
+	let del=parseInt(delay.value);
+	setTimeout(()=>{
+		opDiv.textContent=tx;
+	},del);
+});
